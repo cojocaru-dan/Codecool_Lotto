969eaacc04fdeaaa3424f421546c0d4556def261
appuser <appuser@journey2-86bff4fbf7-ljkb2>
2023-07-26 15:53:37 +0000

    starter project

---
+++ Codecool.Lotto/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Codecool.Lotto;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const int quantity = 5;
+        const int maxNumber = 10;
+
+    }
+
+    #region Game Logic
+
+    #endregion
+
+
+    #region User Input
+
+    #endregion
+
+
+    #region Output
+
+    #endregion
+}
